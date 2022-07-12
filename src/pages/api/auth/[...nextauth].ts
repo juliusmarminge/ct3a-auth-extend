4ad3831fe5781ec5aaefbbe098077c3f132b5600
Julius Marminge <51714798+juliusmarminge@users.noreply.github.com>
2022-07-12 22:01:11 +0200

    initial commit

---
+++ src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,35 @@
+import NextAuth, { type NextAuthOptions } from "next-auth";
+import GithubProvider from "next-auth/providers/github";
+import CredentialsProvider from "next-auth/providers/credentials";
+
+export const authOptions: NextAuthOptions = {
+  // Configure one or more authentication providers
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
+    }),
+    // ...add more providers here
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        name: {
+          label: "Name",
+          type: "text",
+          placeholder: "Enter your name",
+        },
+      },
+      async authorize(credentials, _req) {
+        const user = { id: 1, name: credentials?.name ?? "J Smith" };
+        return user;
+      },
+    }),
+  ],
+  callbacks: {
+    session({ session, token, user }) {
+      return session; // The return type will match the one returned in `useSession()`
+    },
+  },
+};
+
+export default NextAuth(authOptions);
